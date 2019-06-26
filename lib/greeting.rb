0ab83a96882b3832d082e83d0dec5e569befec3c
Christopher Dent <christopherdent01@gmail.com>
2019-06-26 02:41:29 +0000

    Automatically backed up by Learn

--- lib/greeting.rb
+++ lib/greeting.rb
@@ -1,6 +1,6 @@
 # code the #greeting method here!
 def greeting(name)
-  print.strip "Hello #{name}.  It's nice to meet you."
+  print "Hello #{name}.  It's nice to meet you."
 end
 
 greeting("Sally")
